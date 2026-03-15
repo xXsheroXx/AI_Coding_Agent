a3c84f9d7272abe64278695919b4ebee0943c9be
xXsheroXx <shero010807@gmail.com>
2026-03-15 11:03:20 +0100

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,14 @@
 import com.openai.client.OpenAIClient;
 import com.openai.client.okhttp.OpenAIOkHttpClient;
+import com.openai.core.JsonValue;
+import com.openai.models.FunctionDefinition;
+import com.openai.models.FunctionParameters;
 import com.openai.models.chat.completions.ChatCompletion;
 import com.openai.models.chat.completions.ChatCompletionCreateParams;
+import com.openai.models.chat.completions.ChatCompletionTool;
+
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -31,6 +38,7 @@ public class Main {
                 ChatCompletionCreateParams.builder()
                         .model("anthropic/claude-haiku-4.5")
                         .addUserMessage(prompt)
+                        .tools(getAvailableTools())
                         .build()
         );
 
@@ -43,5 +51,32 @@ public class Main {
 
         // TODO: Uncomment the line below to pass the first stage
          System.out.print(response.choices().get(0).message().content().orElse(""));
+    }
+
+    private static List<ChatCompletionTool> getAvailableTools() {
+        var readParameters =
+                FunctionParameters.builder()
+                        .putAdditionalProperty("type", JsonValue.from("object"))
+                        .putAdditionalProperty("properties",
+                                JsonValue.from(
+                                        Map.of(
+                                                "file_path",
+                                                Map.of(
+                                                        "type", "string",
+                                                        "description", "The path to the file to read, relative to the current directory."))))
+                        .build();
+
+        var readTool =
+                ChatCompletionTool.builder()
+                        .type(JsonValue.from("function"))
+                        .function(
+                                FunctionDefinition.builder()
+                                        .name("Read")
+                                        .description("Read and return the contents of a file")
+                                        .parameters(readParameters)
+                                        .build())
+                        .build();
+
+        return List.of(readTool);
     }
 }
