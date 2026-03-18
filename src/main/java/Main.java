e3bc3d7eba12fb8d43759730aa7a3c2bab6c63ed
xXsheroXx <shero010807@gmail.com>
2026-03-18 10:19:15 +0100

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -52,13 +52,13 @@ public class Main {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.err.println("Logs from your program will appear here!");
 
-        ChatCompletion.Choice choice = response.choices().get(0);
-        Optional<List<ChatCompletionMessageToolCall>> toolCalls = choice.message().toolCalls();
-        if (toolCalls.isEmpty()) {
-            System.out.print(choice.message().content().orElse(""));
-        } else {
-            parseTools(toolCalls.get());
-        }
+//        ChatCompletion.Choice choice = response.choices().get(0);
+//        Optional<List<ChatCompletionMessageToolCall>> toolCalls = choice.message().toolCalls();
+//        if (toolCalls.isEmpty()) {
+//            System.out.print(choice.message().content().orElse(""));
+//        } else {
+//            parseTools(toolCalls.get());
+//        }
 
         runLoop(client, paramBuilder);
 
